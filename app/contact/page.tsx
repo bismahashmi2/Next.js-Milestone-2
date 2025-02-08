6e5d5523e80341e17a3df0c980b0034e96c9e7e0
Hashmi Hashmi <119hashmihashmi@gmail.com>
2025-02-08 14:49:16 +0500

    improved code

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="w-full min-h-screen py-40 px-10 leading-relaxed bg-gradient-to-b from-[#fef9c3] via-[#b2d8d8] to-[#fef9c3]">
+    <div className="w-full min-h-screen py-40 px-4 leading-relaxed bg-gradient-to-b from-[#fef9c3] via-[#b2d8d8] to-[#fef9c3]">
       <div className="w-full bg-white max-w-xlg mx-auto rounded-lg shadow-md p-10">
         <h1 className="text-[32px] sm:text-[50px] text-center text-[#D72638] font-bold bg-gradient-to-r from-[#D72638] to-coral-pink bg-clip-text text-transparent">
           Have further queries or feedbacks? Contact us now!
@@ -10,10 +10,10 @@ const Contact = () => {
 
         <form className="bg-peach rounded-lg shadow-lg p-8 sm:p-6 mt-6">
   
-         <div>
+         <div className="mb-4">
             <label className="text-lg font-semibold">Name: </label>
-            <input type="text" className="sm:w-full p-2 lg:max-w-40" placeholder="Enter your name" required />
-            </div> <br />
+            <input type="text" className="p-2" placeholder="Enter your name" required />
+            </div> 
 
                 <div>
                     <label className="text-lg font-semibold">Email: </label>
